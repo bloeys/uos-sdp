1be1ce236d50667417fbf26d1574ed940c83c0e4
bloeys <bloeys@outlook.com>
2021-02-14 11:06:24 +0400

    Organize into functions

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 
 pytesseract.pytesseract.tesseract_cmd = r'D:\Program Files\tesseract-ocr\tesseract.exe'
 
-
 class CharContour():
     def __init__(self, x, y, w, h, cnt):
         self.x = x
@@ -89,9 +88,9 @@ def FilterBoundingBoxes(chars):
     return chars
 
 
-def ContourWithinPlates(x, y, w, h):
+def ContourWithinPlates(plates, x, y, w, h):
 
-    for plate in candidatePlates:
+    for plate in plates:
         if GetPercentOverlap(x, y, w, h, plate[0], plate[1], plate[2], plate[3]) >= 0.4:
             return True
 
@@ -106,7 +105,7 @@ def GetPercentOverlap(x1, y1, w1, h1, x2, y2, w2, h2):
     return overlapArea / (w1 * h1)
 
 
-def OCR(chars):
+def OCR(imgColor, morphedImg, chars):
 
     for c in chars:
         # Highlight the contour we are trying to get characters from
@@ -183,120 +182,121 @@ def ShowImg(img):
     plt.imshow(i)
     plt.show()
 
+def GetPlateNumber(imgName):
 
-imgName = 'plate5.png'
-imgColor = cv2.imread(imgName)
-height, width, channels = imgColor.shape
+    imgColor = cv2.imread(imgName)
+    height, width, _ = imgColor.shape
 
-# Image pre-processing
-img = cv2.imread(imgName, cv2.IMREAD_GRAYSCALE)
-cv2.imshow('1-original', img)
+    # Image pre-processing
+    img = cv2.imread(imgName, cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('1-original', img)
 
-img = cv2.add(img, np.array([50.0]))
-cv2.imshow('2-brightened', img)
-img = cv2.GaussianBlur(img, (3, 3), 0)
-cv2.imshow('2-brightened+blur', img)
+    img = cv2.add(img, np.array([50.0]))
+    cv2.imshow('2-brightened', img)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    cv2.imshow('2-brightened+blur', img)
 
-blackHatMorphKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (19, 7))
-img = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, blackHatMorphKernel)
-morphedImg = img.copy()
-cv2.imshow('3-morph-blackhat', img)
+    blackHatMorphKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (19, 7))
+    img = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, blackHatMorphKernel)
+    morphedImg = img.copy()
+    cv2.imshow('3-morph-blackhat', img)
 
-# Detect plate area and ignore regions not within specific area within image
-plate = cv2.morphologyEx(morphedImg, cv2.MORPH_CLOSE,
-                         cv2.getStructuringElement(cv2.MORPH_RECT, (19, 7)))
-cv2.imshow('4.1-morph-closed', plate)
+    # Detect plate area and ignore regions not within specific area within image
+    plate = cv2.morphologyEx(morphedImg, cv2.MORPH_CLOSE,
+                            cv2.getStructuringElement(cv2.MORPH_RECT, (19, 7)))
+    cv2.imshow('4.1-morph-closed', plate)
 
-plate = cv2.threshold(plate, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-cv2.imshow('4.2-threshold-OTSU', plate)
+    plate = cv2.threshold(plate, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    cv2.imshow('4.2-threshold-OTSU', plate)
 
-plate = cv2.erode(plate, None, iterations=5)
-cv2.imshow('4.3-erode', plate)
+    plate = cv2.erode(plate, None, iterations=5)
+    cv2.imshow('4.3-erode', plate)
 
-plate = cv2.dilate(plate, None, iterations=15)
-cv2.imshow('4.4-threshold-dilate', plate)
+    plate = cv2.dilate(plate, None, iterations=15)
+    cv2.imshow('4.4-threshold-dilate', plate)
 
-cnts2 = cv2.findContours(plate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-cntAreas = []
-for cnt in cnts2:
-    cntAreas.append(cv2.contourArea(cnt))
+    cnts2 = cv2.findContours(plate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    cntAreas = []
+    for cnt in cnts2:
+        cntAreas.append(cv2.contourArea(cnt))
 
-cntsByArea = [x for _, x in sorted(
-    zip(cntAreas, cnts2), key=lambda pair: pair[0], reverse=True)]
-cnts2 = cntsByArea[:10]
+    cntsByArea = [x for _, x in sorted(zip(cntAreas, cnts2), key=lambda pair: pair[0], reverse=True)]
+    cnts2 = cntsByArea[:10]
 
-cps = []
-candidatePlates = []
-for i in range(len(cnts2)-1, -1, -1):
+    cps = []
+    candidatePlates = []
+    for i in range(len(cnts2)-1, -1, -1):
 
-    cnt = cnts2[i]
-    x, y, w, h = cv2.boundingRect(cnt)
+        cnt = cnts2[i]
+        x, y, w, h = cv2.boundingRect(cnt)
 
-    minX = width * 0.3
-    maxX = width * 0.7
+        minX = width * 0.3
+        maxX = width * 0.7
 
-    minY = height * 0.5
-    maxY = height * 0.9
+        minY = height * 0.5
+        maxY = height * 0.9
 
-    posX = x + w * 0.5
-    posY = y + h * 0.5
-    if posX < minX or posX > maxX or posY < minY or posY > maxY:
-        continue
+        posX = x + w * 0.5
+        posY = y + h * 0.5
+        if posX < minX or posX > maxX or posY < minY or posY > maxY:
+            continue
 
-    cps.append(cnt)
-    candidatePlates.append((x, y, w, h))
+        cps.append(cnt)
+        candidatePlates.append((x, y, w, h))
 
-cv2.imshow('4.5-chosen-plates',
-           cv2.drawContours(imgColor.copy(), cps, -1, (255, 0, 0), 2))
-##
+    cv2.imshow('4.5-chosen-plates', cv2.drawContours(imgColor.copy(), cps, -1, (255, 0, 0), 2))
 
-img = cv2.Canny(img, 50, 200)
-cv2.imshow('5-canny-img', img)
+    img = cv2.Canny(img, 50, 200)
+    cv2.imshow('5-canny-img', img)
 
-# Find all contours
-cnts = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+    # Find all contours
+    cnts = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-# Remove 40% smallest contours
-cntAreas = []
-for cnt in cnts:
-    cntAreas.append(cv2.contourArea(cnt))
+    # Remove 40% smallest contours
+    cntAreas = []
+    for cnt in cnts:
+        cntAreas.append(cv2.contourArea(cnt))
 
-cntsByArea = [x for _, x in sorted(
-    zip(cntAreas, cnts), key=lambda pair: pair[0])]
-cnts = cntsByArea[floor(len(cntsByArea) * 0.4):]
+    cntsByArea = [x for _, x in sorted(zip(cntAreas, cnts), key=lambda pair: pair[0])]
+    cnts = cntsByArea[floor(len(cntsByArea) * 0.4):]
 
-# Draw all remaining cnts
-cv2.imshow('dang', cv2.drawContours(imgColor, cnts, -1, (0, 255, 0), 1))
-cv2.waitKey()
+    # Draw all remaining cnts
+    cv2.imshow('dang', cv2.drawContours(imgColor, cnts, -1, (0, 255, 0), 1))
+    cv2.waitKey()
 
-# Filter by AR and create contour objects
-charContours = []
-for i in range(len(cnts)-1, -1, -1):
+    # Filter by AR and create contour objects
+    charContours = []
+    for i in range(len(cnts)-1, -1, -1):
 
-    cnt = cnts[i]
-    x, y, w, h = cv2.boundingRect(cnt)
-    aspectRatio = w / float(h)
-    if not(aspectRatio > 0.15 and aspectRatio < 0.9) or not(ContourWithinPlates(x, y, w, h)):
-        continue
+        cnt = cnts[i]
+        x, y, w, h = cv2.boundingRect(cnt)
+        aspectRatio = w / float(h)
+        if not(aspectRatio > 0.15 and aspectRatio < 0.9) or not(ContourWithinPlates(candidatePlates, x, y, w, h)):
+            continue
 
-    # Expand rect a bit so the detector works
-    offset = 2
-    x = Clamp(x-offset, 0, width)
-    y = Clamp(y-offset, 0, height)
-    w = Clamp(w+offset*2, 0, width)
-    h = Clamp(h+offset*2, 0, height)
+        # Expand rect a bit so the detector works
+        offset = 2
+        x = Clamp(x-offset, 0, width)
+        y = Clamp(y-offset, 0, height)
+        w = Clamp(w+offset*2, 0, width)
+        h = Clamp(h+offset*2, 0, height)
 
-    charContours.append(CharContour(x, y, w, h, cnt))
+        charContours.append(CharContour(x, y, w, h, cnt))
 
-# Remove non-external bounding boxes
-charContours = FilterBoundingBoxes(charContours)
-OCR(charContours)
-charContours = FilterOCR(charContours)
+    # Remove non-external bounding boxes
+    charContours = FilterBoundingBoxes(charContours)
+    OCR(imgColor, morphedImg, charContours)
+    charContours = FilterOCR(charContours)
 
-charContours = sorted(charContours, key=lambda c: c.x)
-print('Extracted digits ordered by x-position:', charContours)
+    charContours = sorted(charContours, key=lambda c: c.x)
+    print('Extracted digits ordered by x-position:', charContours)
 
-carLetter, carNum, carCity = GetCarInfo(charContours)
-print('Detected car letter:', carLetter)
-print('Detected car number:', carNum)
-cv2.waitKey(0)
+    carLetter, carNum, carCity = GetCarInfo(charContours)
+    cv2.waitKey(0)
+
+    return carLetter, carNum, carCity
+
+if __name__ == '__main__':
+    letter, number, _ = GetPlateNumber('plate1.png')
+    print('\nDetected car letter:', letter)
+    print('Detected car number:', number)
