e4018ec4ea6dc38a625baa41e12bf86f6df9c7e3
bloeys <bloeys@outlook.com>
2021-02-14 11:55:41 +0400

    Basic server

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import base64
+from anpr import GetPlateNumber
+
+app = Flask(__name__)
+
+@app.route('/isVehicleAuthorized', methods=['POST'])
+def isVehicleAuthorized():
+
+    with open('imgToTest.png', "wb") as fh:
+        fh.write(base64.b64decode(request.json['image']))
+
+    letter, num, _ = GetPlateNumber('imgToTest.png')
+    print('\nDetected car letter:', letter)
+    print('Detected car number:', num)
+    
+    return {"authorized": True}
