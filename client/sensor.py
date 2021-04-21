8c6ab5e405e0e29001b1272aeb618ea924220af9
bloeys <bloeys@outlook.com>
2021-04-21 10:20:34 +0400

    Protect against infinite loop in sensor

--- client/sensor.py
+++ client/sensor.py
@@ -29,14 +29,20 @@ def getDistanceCM(sideSensor = False):
     # Calculate time receiving a high signal
     
     #GPIO.wait_for_edge(echoPin, GPIO.RISING)
+    readStart = time.time()
     startTime = time.time()
     while GPIO.input(echoPin) == GPIO.LOW:
         startTime = time.time()
+        if time.time() - readStart > 3:
+          return 1000
  
     #GPIO.wait_for_edge(echoPin, GPIO.FALLING)
+    readStart = time.time()
     stopTime = time.time()
     while GPIO.input(echoPin) == GPIO.HIGH:
         stopTime = time.time()
+        if time.time() - readStart > 3:
+          return 1000
 
     pulseWidth = stopTime - startTime
     return (pulseWidth * 340) / 2 * 100
